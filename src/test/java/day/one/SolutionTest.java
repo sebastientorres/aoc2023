8e26678a2326cae34b3335b427cecf618a6a53a2
Sebastien Torres <storres@numarkdigital.co.uk>
2023-12-01 11:18:39 +0000

    Tests...

---
+++ src/test/java/day/one/SolutionTest.java
@@ -0,0 +1,33 @@
+package day.one;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    private static final String path = "src/test/resources/day/one";
+    private final Solution solution = new Solution();
+
+    @Test
+    public void partOne() {
+        var file = new File(path+"/inputPartOne");
+
+        var actual = solution.partOne(file);
+        assertEquals(77, actual);
+
+    }
+
+    @Test
+    public void partTwo() {
+        var file = new File(path+"/inputPartTwo");
+
+        var actual = solution.partTwo(file);
+
+        assertEquals(281, actual);
+
+    }
+
+}

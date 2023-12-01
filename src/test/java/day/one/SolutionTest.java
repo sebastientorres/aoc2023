e8d25b4e3cf482a16e63e0d018ab392aefbd2f80
Sebastien Torres <baztorres@gmail.com>
2023-12-01 19:43:40 +0000

    Correct part one file based test.  Test the line converter method

--- src/test/java/day/one/SolutionTest.java
+++ src/test/java/day/one/SolutionTest.java
@@ -1,10 +1,16 @@
 package day.one;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 class SolutionTest {
 
@@ -15,7 +21,13 @@ class SolutionTest {
     public void partOne() {
         var file = new File(path+"/inputPartOne");
         var actual = solution.partOne(file);
-        assertEquals(77, actual);
+        assertEquals(142, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void partOneLineDecoder(String input, int expected) {
+        assertEquals(expected, solution.partOneLineDecoder(input));
     }
 
     @Test
@@ -23,6 +35,33 @@ class SolutionTest {
         var file = new File(path+"/inputPartTwo");
         var actual = solution.partTwo(file);
         assertEquals(281, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void partTwoLineDecoder(String input, int expected) {
+        assertEquals(expected, solution.partTwoLineDecoder(input));
+    }
+
+    static Stream<Arguments> partOneLineDecoder() {
+        return Stream.of(
+                arguments("1abc2", 12),
+                arguments("pqr3stu8vwx", 38),
+                arguments("a1b2c3d4e5f", 15),
+                arguments("treb7uchet", 77)
+        );
+    }
+
+    static Stream<Arguments> partTwoLineDecoder() {
+        return Stream.of(
+                arguments("two1nine", 29),
+                arguments("eightwothree", 83),
+                arguments("abcone2threexyz", 13),
+                arguments("xtwone3four", 24),
+                arguments("4nineeightseven2", 42),
+                arguments("zoneight234", 14),
+                arguments("7pqrstsixteen", 76)
+        );
     }
 
 }
